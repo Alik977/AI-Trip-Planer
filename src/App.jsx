ee9d9536e43e7430ff70f073804f18e0b860d86f
Alik977 <alik.hakobyan.sh@gmail.com>
2026-08-27 02:37:07 +0400

    route

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
-import { useState } from 'react'
-import Navbar from './components/Navbar.jsx'
-import Home from '../pages/Home.jsx'
-import './App.css'
+import { useState } from "react";
+import Navbar from "./components/Navbar.jsx";
+import Home from "../pages/Home.jsx";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
 
 function App() {
-  
   return (
-    <>
+    <BrowserRouter>
       <Navbar />
-        <Home/>
-      
-    </>
-  )
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,30 @@
 import "./Global.css";
 import logo from "../assets/img/Logo.png";
+import { NavLink } from "react-router-dom";
 function Navbar() {
   return (
     <div className="Navbar">
       <div className="navbarlogo">
         <img src={logo} className="imglogo" alt="" />
       </div>
-      
-        <div className="navbarlist">
+
+      <div className="navbarlist">
+        <NavLink className="list" to="/home">
+          <li>Home</li>
+        </NavLink>
+        <NavLink className="list" to="/Dest">
+          {" "}
           <li>Destination</li>
+        </NavLink>
+        <NavLink className="list" to="/Trip">
           <li>Trip Planner</li>
+        </NavLink>
+        <NavLink className="list" to="/About">
           <li>About</li>
+        </NavLink>
+        <NavLink className="list" to="/contact">
           <li>Contact</li>
-       
+        </NavLink>
       </div>
 
       <div className="navbarbutton">
